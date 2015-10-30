aa574176c66bc290d7dc83ad0bbaaa24788b00a4
Pedro Moreira <pmoreira.estg@gmail.com>
2015-10-30 17:13:50 +0000

    servechape api json + xml

--- serveChave.php
+++ serveChave.php
@@ -1,5 +1,14 @@
 <?php
-header("Content-type: application/json");
+
+if (!isset($_GET['type'])) {
+	$type = 1; // json 
+} else if ($_GET['type'] == 'xml') {
+	$type = 2; // xml
+} else {
+	$type = 1; // json
+}
+
+
 
 class Chave {
 	public $numeros = array();
@@ -37,12 +46,33 @@ class Gerador {
 	public function toJSON() {
 		return json_encode($this->chave);
 	}
+	
+	public function toXML() {
+		$rootXML = new SimpleXMLElement("<chave/>");
+		$xn = $rootXML->addChild("numeros");
+		foreach($this->chave->numeros as $numero) {
+			$xn->addChild("num",$numero);
+		}
+		$xe = $rootXML->addChild("estrelas");
+		foreach($this->chave->estrelas as $numero) {
+			$xe->addChild("num",$numero);
+		}
+		return $rootXML->asXML();
+	}
 }
 
 //echo '{"numeros":[1,2,3,45,50], "estrelas":[10,11]}';
 
 $g = new Gerador();
-$chaveJSON = $g->toJSON();
-echo $chaveJSON;
 
+if ($type == 1) {
+	header("Content-type: application/json");
+	$chave = $g->toJSON();
+} else if ($type == 2) {
+	header("Content-type: text/xml");
+	$chave = $g->toXML();
+} else {
+	echo "está tudo maluco!!!!";
+}
+echo $chave;
 ?>
